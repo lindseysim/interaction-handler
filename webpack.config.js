f0e2d1cb28913950d68819c60a5c1508505f430b
Lawrence Sim <lawrence.sim.geo@gmail.com>
2022-01-15 12:18:23 -0800

    update/modernize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const webpack = require('webpack'), 
       path    = require('path');
+
 module.exports = {
     entry: './interaction-handler.js', 
     mode: 'production', 
     output: {
-        library: 'InteractionHandler', 
-        libraryTarget: 'this', 
-        libraryExport: 'default', 
+        library: {
+            name:   'InteractionHandler', 
+            type:   'this', 
+            export: 'default'
+        }, 
         path: path.resolve(__dirname), 
         filename: 'interaction-handler.min.js'
     },
@@ -16,9 +19,7 @@ module.exports = {
                 test:    /\.js$/,
                 exclude: /(node_modules)/,
                 loader:  'babel-loader', 
-                query: {
-                    presets: ['@babel/preset-env']
-                }
+                options: {presets: ['@babel/preset-env']}
             }
         ]
     }, 
